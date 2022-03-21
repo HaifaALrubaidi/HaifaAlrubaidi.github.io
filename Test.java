2fa9d513218b3b79ac42b5d13af01b81dc891d7f
HaifaALrubaidi <97825246+HaifaALrubaidi@users.noreply.github.com>
2022-03-21 12:25:56 +0300

    Add files via upload

--- Test.java
+++ Test.java
@@ -1,21 +1,18 @@
-import java.util.ArrayList;
-import java.util.Scanner;
-
 /**
- * Created by MC on 30/01/2022.
+ * Created by MC on 19/02/2022.
  */
-public class Test {
+public class Test
+{
     public static void main(String[] args) {
-        ArrayQueue<Character>Q=new ArrayQueue<>(5);
-        Scanner in =new Scanner(System.in);
-        System.out.println("Enter Queue element");
-        for (int i = 0; i <5 ; i++) {
-           Q.enqueue(in.next().charAt(0));
-            System.out.println(" first  "+Q.first());
-            System.out.println("size ="+Q.size());
-        }
-        System.out.println("all element are");
-       while (!Q.isEmpty())
-           System.out.print(Q.dequeue()+"\t");
+        SingleLinkedList<String>list=new SingleLinkedList<>();
+        System.out.println(list.size);
+        list.addfirst("Haifa");
+        list.addlast("Safa");
+        System.out.println(list.size);
+        System.out.println(list.first());
+        System.out.println(list.last());
+        while (!list.isEmpty())
+            list.removefrist();
+        System.out.println(list.size);
     }
 }
